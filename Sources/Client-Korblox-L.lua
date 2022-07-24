c16133ddc56749c29143a6941892e8773bc1f045
lpxy <109869788+lpxy@users.noreply.github.com>
2022-07-25 01:36:11 +0800

    Create Client-Korblox-L.lua

---
+++ Sources/Client-Korblox-L.lua
@@ -0,0 +1,8 @@
+local LP = game.Players.LocalPlayer
+		local Char = LP.Character
+		Char.LeftLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942101"
+		Char.LeftLowerLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+		Char.LeftUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942082"
+		Char.LeftUpperLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+		Char.LeftFoot.MeshId = "902942089"
+		Char.LeftFoot.Transparency = "1"
